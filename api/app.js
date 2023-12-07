a505a8c2437be3e2f95d6e62fb2500da5259926c
Cliff Sanchez <kenshinmax@gmail.com>
2023-12-06 19:41:11 -0500

    adding Kompose for k8s deployment

--- api/app.js
+++ api/app.js
@@ -3,10 +3,11 @@ const cors = require('cors')
 
 const app = express()
 
-const OTEL_SERVICE_NAME='inventory'
+const OTEL_SERVICE_NAME='inventory-api'
 const OTEL_RESOURCE_ATTRIBUTES='deployment.environment=dev, service.version=1.0.1'
 const OTEL_EXPORTER_OTLP_ENDPOINT='http://localhost:4317'
 const OTEL_LOG_LEVEL='debug'
+
 app.use(cors())
 
 app.get('/', (req, res) => {
